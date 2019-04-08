fdc7e42cf83cb4187ffc154766005113a5569dc5
Majid <mohammadmajid55@gmail.com>
2019-04-08 21:26:37 +0000

    done uppercase

---
+++ step6/print_uppercase.js
@@ -0,0 +1,11 @@
+var getHTML = require('./http-functions');
+
+function printUpperCase(html) {
+    console.log(html.toUpperCase());
+  }
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+getHTML(requestOptions,printUpperCase);
